e1f6c762be2e36d2ccf6f2559dee4c40f622fc0a
Sidaoui <mssidaoui@gmail.com>
2023-06-05 08:19:58 +0100

    finish_project

--- spring-boot-test-project/src/test/java/com/example/demo/student/StudentServiceTest.java
+++ spring-boot-test-project/src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,15 +1,22 @@
 package com.example.demo.student;
 
+import com.example.demo.student.exception.BadRequestException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 
@@ -45,12 +52,55 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
     void addStudent() {
+        // Given
+        String email = "mssidaoui@gmail.com";
+        Student student = new Student(
+                "Sidaoui",
+                email,
+                Gender.MALE
+        );
+        underTest.addStudent(student);
+        // Then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
+    }
+    @Test
+    void willThrowWhenEmailIsTaken() {
+        // Given
+        String email = "mssidaoui@gmail.com";
+        Student student = new Student(
+                "Sidaoui",
+                email,
+                Gender.MALE
+        );
+        given(studentRepository.selectExistsEmail (student.getEmail()))
+                .willReturn(true);        // Then
+                assertThatThrownBy(() -> underTest.addStudent(student))
+        .isInstanceOf(BadRequestException.class)
+        .hasMessageContaining ("Email " + student.getEmail() + " taken");
+
+        verify(studentRepository, never()).save(any());
     }
 
     @Test
     @Disabled
     void deleteStudent() {
+        //given
+        String email = "mssidaoui@gmail.com";
+        Student student = new Student(
+                "Sidaoui",
+                email,
+                Gender.MALE
+        );
+
+        //when
+
+        //then
     }
 }
